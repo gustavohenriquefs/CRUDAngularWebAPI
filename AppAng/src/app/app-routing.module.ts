c1dba9c92ca289351013d5cf45184e7edefe20aa
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2021-09-17 11:31:44 -0300

    adicionando o bootstrap e o componente pessoas

--- AppAng/src/app/app-routing.module.ts
+++ AppAng/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PessoasComponent } from './components/pessoas/pessoas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "pessoas",
+  component: PessoasComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
--- AppAng/src/app/app.module.ts
+++ AppAng/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoasService } from './pessoas.service';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { PessoasComponent } from './components/pessoas/pessoas.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PessoasComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PessoasService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
