5717073878e3840cbff01a10af6513b4660bcf68
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2021-09-17 10:21:58 -0300

    modificação do projeto angular

---
+++ temp/components/pessoas/pessoas.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-pessoas',
+  templateUrl: './pessoas.component.html',
+  styleUrls: ['./pessoas.component.scss'],
+})
+export class PessoasComponent implements OnInit {
+  formulario: any;
+  tituloFormulario: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.tituloFormulario = "Nova Pessoa"
+    this.formulario = new FormGroup({
+      nome: new FormControl(null),
+      sobrenome: new FormControl(null),
+      idade: new FormControl(null),
+      profissao: new FormControl(null)
+    });
+  }
+
+
+}
